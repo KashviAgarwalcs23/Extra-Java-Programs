f9dd745f6f88e8d13608d834b4ef3854d1b704c7
KashviAgarwalcs23 <kashviagarwal.cs23@bmsce.ac.in>
2024-09-30 18:46:33 +0530

    Add files via upload
    This is an extra java program done on 30 September,2024.
    This is Promote program.

---
+++ promote.java
@@ -0,0 +1,15 @@
+class promote{
+public static void main(String args[]){
+byte b = 42;
+int i =5000;
+short s = 1024;
+char c  ='a';
+float f = 6.74f;
+double d = 0.1234;
+double result = (f*b)+(i/c)-(d*s);
+System.out.println((f*b)+ " + " + (i/c) + " - " + (d*s));
+
+System.out.println("Name : Kashvi Agarwal");
+System.out.println("USN : 1BM23CS142");
+}
+}
