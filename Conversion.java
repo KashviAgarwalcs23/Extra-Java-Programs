2b6e2e1aee3d0ecc828411fcf43527787fc3b67c
KashviAgarwalcs23 <kashviagarwal.cs23@bmsce.ac.in>
2024-09-30 18:36:48 +0530

    Add files via upload
    This is the extra Java programs done on 30 September,2024.
    This is Conversion Program

---
+++ Conversion.java
@@ -0,0 +1,18 @@
+class Conversion{
+public static void main(String args[]){
+short s1 = 1000,s2;
+int i1 = 100000,i2;
+byte b =1;
+long l1 = 10000000,l2;
+char c = '+';
+float f1 = 25.69f,f2;
+double d1 = 536987.125,d2;
+System.out.println(b + " " + s1 + " " + l1 + " " + c + " " + f1 + " " + d1);
+s2 = b;
+i2 = s2;
+l2 = i1;
+System.out.println(s2 + " " + i2 + " " + l2);
+System.out.println("Name : Kashvi Agarwal");
+System.out.println("USN : 1BM23CS142");
+}
+}
