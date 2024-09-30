1c2e1f40181eb78f37742ec3a0eea9edee9de775
KashviAgarwalcs23 <kashviagarwal.cs23@bmsce.ac.in>
2024-09-30 18:51:20 +0530

    Add files via upload
    This is an extra Java Program done on 30 September,2024.
    This is a Widening Program.

---
+++ widening.java
@@ -0,0 +1,19 @@
+class widening{
+public static void main(String args[]){
+short s;
+int i1,i2;
+byte b1 =10, b2 = 20;
+s = b1;
+i1 = b2;
+System.out.println("byte to short conversion");
+System.out.println(b1 + " " + s);
+System.out.println("byte to int conversion");
+System.out.println(b2 + " " + i1);
+char c = 'a';
+i2 = c;
+System.out.println("char to int conversion");
+System.out.println(c+" " + i2);
+System.out.println("Name : Kashvi Agarwal");
+System.out.println("USN : 1BM23CS142");
+}
+}
